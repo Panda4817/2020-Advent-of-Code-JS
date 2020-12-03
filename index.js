affb3d538da7b199538663cd541ecaaa5ebc4d0e
K Munton <kanta.munton@gmail.com>
2020-12-03 13:30:10 +0000

    completed day 3

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ const runner = require("./runner");
 // the argument for maxDay is optional, remove it for no limit
 // runner.getDay(5).then((day) => runner.run(day))
 
-runner.run(day=2);
+runner.run(day=3);
 
 ("");
