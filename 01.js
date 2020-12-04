623a402bc770ac80d8c18e134fa9b819642910ba
K Munton <kanta.munton@gmail.com>
2020-12-04 21:36:18 +0000

    completed day 4

--- 01.js
+++ 01.js
@@ -1,52 +1,44 @@
 const part1 = data =>
   new Promise(resolve => {
-    setTimeout(() => {
-      const num  = Array.from(data.split("\n"))
-      const len = num.length
-      const target = 2020
-      let num1 = 0
-      let num2 = 0
-      for (let i=0; i<len; i ++) {
-        for (let j=i+1; j<len; j ++) {
-          if (parseInt(num[i]) + parseInt(num[j]) == target && i != j) {
-            num1 = parseInt(num[i])
-            num2 = parseInt(num[j])
-            console.log(num1, num2)
-          }
+    const num  = Array.from(data.split("\n"))
+    const len = num.length
+    const target = 2020
+    let num1 = 0
+    let num2 = 0
+    for (let i=0; i<len; i ++) {
+      for (let j=i+1; j<len; j ++) {
+        if (parseInt(num[i]) + parseInt(num[j]) == target && i != j) {
+          num1 = parseInt(num[i])
+          num2 = parseInt(num[j])
         }
       }
-      
-      resolve(num1*num2)
-      
-    }, 1000);
+    }
+    
+    resolve(num1*num2)
   });
 
 const part2 = data =>
   new Promise(resolve => {
-    setTimeout(() => {
-      const num  = Array.from(data.split("\n"))
-      const len = num.length
-      const target = 2020
-      let num1 = 0
-      let num2 = 0
-      let num3 = 0
-      for (let i=0; i<len; i ++) {
-        for (let j=i+1; j<len; j ++) {
-          for (let k=j+1; k<len; k++) {
-            if (parseInt(num[i]) + parseInt(num[j]) + parseInt(num[k]) == target && i != j != k) {
-            num1 = parseInt(num[i])
-            num2 = parseInt(num[j])
-            num3 = parseInt(num[k])
-            console.log(num1, num2, num3)
-          }
-          }
-          
+    const num  = Array.from(data.split("\n"))
+    const len = num.length
+    const target = 2020
+    let num1 = 0
+    let num2 = 0
+    let num3 = 0
+    for (let i=0; i<len; i ++) {
+      for (let j=i+1; j<len; j ++) {
+        for (let k=j+1; k<len; k++) {
+          if (parseInt(num[i]) + parseInt(num[j]) + parseInt(num[k]) == target && i != j != k) {
+          num1 = parseInt(num[i])
+          num2 = parseInt(num[j])
+          num3 = parseInt(num[k])
         }
+        }
+        
       }
-      
-      resolve(num1*num2*num3)
-      
-    }, 1000);
+    }
+    
+    resolve(num1*num2*num3)
   });
 
 module.exports = {
--- 02.js
+++ 02.js
@@ -1,53 +1,47 @@
 const part1 = data =>
   new Promise(resolve => {
-    setTimeout(() => {
-      const passwords  = data.split("\n");
-      let overall = 0;
-      for (let i=0; i<passwords.length; i++) {
-        const parts = passwords[i].split(" ");
-        const pol1_pol2 = parts[0].split("-");
-        const letter = parts[1].split(":")[0];
-        console.log(parts[2], pol1_pol2, letter)
-        let count = 0;
-        for (let j=0; j<parts[2].length; j++) {
-          if (parts[2][j] == letter) {
-            count ++;
-          }
-        }
-        if (count >= parseInt(pol1_pol2[0]) && count <= parseInt(pol1_pol2[1])){
-          overall ++;
+    const passwords  = data.split("\n");
+    let overall = 0;
+    for (let i=0; i<passwords.length; i++) {
+      const parts = passwords[i].split(" ");
+      const pol1_pol2 = parts[0].split("-");
+      const letter = parts[1].split(":")[0];
+      let count = 0;
+      for (let j=0; j<parts[2].length; j++) {
+        if (parts[2][j] == letter) {
+          count ++;
         }
       }
-      resolve(overall);
-    }, 1000);
+      if (count >= parseInt(pol1_pol2[0]) && count <= parseInt(pol1_pol2[1])){
+        overall ++;
+      }
+    }
+    resolve(overall);
   
   });
 
 const part2 = data =>
   new Promise(resolve => {
-    setTimeout(() => {
-      const passwords  = data.split("\n");
-      let overall = 0;
-      for (let i=0; i<passwords.length; i++) {
-        const parts = passwords[i].split(" ");
-        const pol1_pol2 = parts[0].split("-");
-        const letter = parts[1].split(":")[0];
-        const pos = [parseInt(pol1_pol2[0]) - 1, parseInt(pol1_pol2[1]) - 1];
-        console.log(parts[2], pos, letter)
-        try{
-          if (parts[2][pos[0]] == letter && parts[2][pos[1]] != letter) {
-            overall ++;
-          } else if (parts[2][pos[0]] != letter && parts[2][pos[1]] == letter) {
-            overall ++;
-          } else {
-            continue;
-          }
-        }catch(IndexError) {
+    const passwords  = data.split("\n");
+    let overall = 0;
+    for (let i=0; i<passwords.length; i++) {
+      const parts = passwords[i].split(" ");
+      const pol1_pol2 = parts[0].split("-");
+      const letter = parts[1].split(":")[0];
+      const pos = [parseInt(pol1_pol2[0]) - 1, parseInt(pol1_pol2[1]) - 1];
+      try{
+        if (parts[2][pos[0]] == letter && parts[2][pos[1]] != letter) {
+          overall ++;
+        } else if (parts[2][pos[0]] != letter && parts[2][pos[1]] == letter) {
+          overall ++;
+        } else {
           continue;
         }
+      }catch(IndexError) {
+        continue;
       }
-      resolve(overall);
-    }, 1000);
+    }
+    resolve(overall);
 
   });
 
